9df7235c120da52c80b7d5814fb3d57598ad5768
24DP2JGala <A240421JG@rvt.lv>
2025-09-16 10:18:15 +0300

    Uzdevums "Produkts"

---
+++ src/main/java/rvt/Product.java
@@ -0,0 +1,31 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Stundas_darbs {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Produkts (nosaukums):");
+
+        String produkts = reader.nextLine();
+
+        System.out.println("Produktu skaits:");
+
+        int skaits = reader.nextInt();
+
+        System.out.println("Produkta cena:");
+
+        double cena = reader.nextDouble();
+        
+        System.out.println();
+        System.out.println("Produkts:");
+        System.out.println(produkts);
+        System.out.println("Cena:");
+        System.out.println(cena);
+        System.out.println("Daudzums:");
+        System.out.println(skaits);
+        System.out.println("totalPrice:");
+        System.out.println(skaits * cena);
+    }
+}
