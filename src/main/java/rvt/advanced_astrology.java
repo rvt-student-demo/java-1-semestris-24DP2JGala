7b3789b32132ea90e99e3a582826838c0621139b
24DP2JGala <A240421JG@rvt.lv>
2025-10-09 13:18:05 +0300

    Advanced astrology (3 parts)

---
+++ src/main/java/rvt/advanced_astrology.java
@@ -0,0 +1,43 @@
+package rvt;
+
+public class advanced_astrology {
+    public static void main(String[] args) {
+        christmasTree(10);
+    }
+
+    public static void printSpaces(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print(" ");
+        }
+    }
+
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+
+        System.out.println();
+    }
+
+    public static void printTriangle(int size) {
+        for (int i = 0; i <= size; i++) {
+            printSpaces(size - i);
+            printStars(i);
+        }
+    }
+
+    public static void christmasTree(int height) {
+        for (int i = 0; i <= height; i++ ) {
+
+            printSpaces(height - i);
+            printStars(i*2 - 1);
+        }
+
+        for (int i = 0; i != 2; i++) {
+            printSpaces(height - 2);
+            printStars(3);
+        }
+
+        System.out.println();
+    }
+}
